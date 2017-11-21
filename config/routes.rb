9d6e0209e518cb391fde67bc8145ac019113ca18
rabea bader <bdr193@gmail.com>
2017-11-21 12:12:19 +0100

    Facebook login works

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   mount Attachinary::Engine => "/attachinary"
   resources :bikes
-  devise_for :users
+  devise_for :users,
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :users do
     resources :bookings, only: [:index, :show, :new, :create, :destroy]
   end
