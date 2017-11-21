bfa0e3872e0341d3fc3cbde09339ef38765809b8
Stephan Schmidbauer <schmidbauer.stephan@gmail.com>
2017-11-21 12:58:46 +0100

    seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+bikes_attributes = [
+  {
+    address: Faker::Address.street_address,
+    city: Faker::Address.city,
+    name: Faker::StarWars.character,
+    price_by_day: (0..100).to_a.sample,
+    category: Faker::Pokemon.move ,
+    user_id: 1
+  },
+        ]
+
+users_attributes = [
+  {
+    email: "#{Faker::Pokemon.name}@gmail.com" ,
+    password: Faker::Team.creature
+  },
+        ]
+
+1.times do
+User.create!(users_attributes)
+end
+
+10.times do
+Bike.create!(bikes_attributes)
+end
+
