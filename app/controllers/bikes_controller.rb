ff541195c488e7a5b39d821f359acbcf0c5437f9
Rich Ogrady <rjogrady21@gmail.com>
2017-11-22 14:27:54 +0100

    Basic map installed with popup info window

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -10,11 +10,17 @@ class BikesController < ApplicationController
     # @bikes = Bike.all.where(city: @city && :end_date >= @start_date)
     # city: @city &&
     # && :end_date >= @start_date
-    @bikes = Bike.all.where("city = :city  AND
-                            start_date <= :start_date AND
-                            end_date >= :start_date AND
-                            end_date >= :end_date",
-                            {city: @city, start_date: @start_date, end_date: @end_date})
+    # @bikes = Bike.all.where("city = :city  AND
+    #                          start_date <= :start_date AND
+    #                          end_date >= :start_date AND
+    #                          end_date >= :end_date",
+    #                          {city: @city, start_date: @start_date, end_date: @end_date})
+    @bikes_with_coordinates = Bike.where.not(latitude: nil, longitude: nil)
+    @hash = Gmaps4rails.build_markers(@bikes_with_coordinates) do |bike, marker|
+      marker.lat bike.latitude
+      marker.lng bike.longitude
+      marker.infowindow render_to_string(partial: "/bikes/map_box", locals: { bike: bike })
+    end
   end
 
   def show
