dc4d0d7cb1e652f3075a9146e0f93ae047421dbc
rabea bader <bdr193@gmail.com>
2017-11-22 17:31:45 +0100

    Demo

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -3,16 +3,15 @@ class BikesController < ApplicationController
   before_action :find_bike_id, only: [:show, :edit, :destroy, :update]
 
   def index
-    # @city = params[:bike][:city]
-    # @start_date = params[:bike][:start_date].to_date
-    # @end_date = params[:bike][:end_date].to_date
-    # @city.downcase!
-    @bikes = Bike.all
-    # @bikes = Bike.all.where("city = :city  AND
-    #   start_date <= :start_date AND
-    #   end_date >= :start_date AND
-    #   end_date >= :end_date",
-    #   {city: @city, start_date: @start_date, end_date: @end_date})
+    @city = params[:bike][:city]
+    @start_date = params[:bike][:start_date].to_date
+    @end_date = params[:bike][:end_date].to_date
+    @city.downcase!
+    @bikes = Bike.all.where("city = :city  AND
+      start_date <= :start_date AND
+      end_date >= :start_date AND
+      end_date >= :end_date",
+      {city: @city, start_date: @start_date, end_date: @end_date})
 
       @bikes_with_coordinates = Bike.where.not(latitude: nil, longitude: nil)
       @hash = Gmaps4rails.build_markers(@bikes_with_coordinates) do |bike, marker|
