7815952b49f4a4cda08cd18a466cf9ac4dd9ec34
Rich Ogrady <rjogrady21@gmail.com>
2017-11-22 15:34:36 +0100

    Seed file created with users and bikes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121104746) do
+ActiveRecord::Schema.define(version: 20171121170211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 20171121104746) do
     t.integer  "user_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.date     "start_date"
+    t.date     "end_date"
     t.index ["user_id"], name: "index_bikes_on_user_id", using: :btree
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,38 +10,110 @@ puts "Clearing database"
 User.destroy_all
 Bike.destroy_all
 
+User.create!(
+  email: "rabea_bader@gmail.com",
+  password: "secret123",
+  first_name: "Rabea",
+  last_name: "Badea"
+)
+
+User.create!(
+  email: "sarah_lafer@yahoo.com",
+  password: "secret123",
+  first_name: "Sarah",
+  last_name: "Lafer"
+)
+
+User.create!(
+  email: "stephan_is_great@gmail.com",
+  password: "secret123",
+  first_name: "Stephan",
+  last_name: "Schmidbauer"
+)
+
+User.create!(
+  email: "richard_ogrady@gmail.com",
+  password: "secret123",
+  first_name: "Richard",
+  last_name: "O'Grady"
+)
+
+User.create!(
+  email: "david_beckham@hotmail.com",
+  password: "secret123",
+  first_name: "David",
+  last_name: "Beckham"
+)
+
+Bike.create!(
+  address: "Schützenstraße 51, 10117 Berlin",
+  city: "berlin",
+  name: "Fantastic 19th century bike",
+  price_by_day: 23,
+  category: "Vintage Bike",
+  start_date: '21-11-2017',
+  end_date: '30-11-2017',
+  user_id: 1,
+  # latitude: 52.509073,
+  # longitude: 13.395794,
+  photo: "https://www.lekkerbikes.com/wp-content/uploads/2017/01/Lekker-Sportief-Womens-Bike-Pastel-Blue.jpg"
+  )
+
+Bike.create!(
+  address: "Axel-Springer-Straße 41, 10969 Berlin",
+  city: "berlin",
+  name: "Dodgy looking modern bike",
+  price_by_day: 3,
+  category: "Mountain Bike",
+  start_date: '21-11-2017',
+  end_date: '30-11-2017',
+  user_id: 2,
+  # latitude: 52.507535,
+  # longitude: 13.399219,
+  photo: "http://rcmedulin.eu/wp-content/uploads/kids-bike-24-2-900-2-400x284.jpg"
+  )
+
+Bike.create!(
+  address: "26 Rudi-Dustchke-Strasse, 10969 Berlin",
+  city: "berlin",
+  name: "Funky fresh bike",
+  price_by_day: 14,
+  category: "Child's Bike",
+  start_date: '21-11-2017',
+  end_date: '30-11-2017',
+  user_id: 3,
+  # latitude: 52.506897,
+  # longitude: 13.391423,
+  photo: "https://static3.nordic.pictures/1172944-thickbox_default/girls-bicycle-disney-frozen-16-inch-volare.jpg"
+  )
+
+Bike.create!(
+  address: "Charlottenstraße 85-96, 10969 Berlin",
+  city: "berlin",
+  name: "The best bike in Berlin",
+  price_by_day: 100,
+  category: "Road Bike",
+  start_date: '21-11-2017',
+  end_date: '30-11-2017',
+  user_id: 4,
+  # latitude: 52.505938,
+  # longitude: 13.392723,
+  photo: "https://cdn.mangobikes.com/wp-content/uploads/custom-ss.jpg"
+  )
+
+Bike.create!(
+  address: "Schützenstraße 8, 10117 Berlin",
+  city: "berlin",
+  name: "A just above average bike",
+  price_by_day: 15,
+  category: "Road Bike",
+  start_date: '21-11-2017',
+  end_date: '30-11-2017',
+  user_id: 5,
+  # latitude: 52.508646,
+  # longitude: 13.392961,
+  photo: "http://www.studds.com.au/bikes/images/2016-studds-100-FB-road-bike-black-lar.jpg"
+  )
+
+
 puts "Populating DB"
-def create_bike(user)
-  bikes_attributes = [
-    {
-      address: Faker::Address.street_address,
-      city: Faker::Address.city,
-      name: Faker::StarWars.character,
-      price_by_day: (0..100).to_a.sample,
-      category: Faker::Pokemon.move ,
-      user_id: user[0][:id]
-    },
-  ]
-end
-
-def create_user
-  users_attributes = [
-    {
-      email: Faker::Internet.email,
-      password: "greatStephan1"
-    },
-  ]
-end
-
-
-
-puts "Creating users and bikes"
-10.times do
-  user = User.create!(create_user)
-  Bike.create!(create_bike(user))
-end
-
-puts "#{User.all.length} Users created"
-puts "#{Bike.all.length} Bikes created"
-
-puts "Finished"
