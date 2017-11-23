2dd046cd1608b0e8b248abe10209aee39e1656e1
rabea bader <bdr193@gmail.com>
2017-11-23 11:09:41 +0100

    Added pictures to users in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,51 +7,59 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 puts "Clearing database"
+puts "Clearing users"
 User.destroy_all
+puts "Clearing bikes"
 Bike.destroy_all
+puts "Clearing bookings"
+Booking.destroy_all
 
-puts "hello"
-
+puts "Creating users"
 user_one = User.create!(
   email: "rabea_bader@gmail.com",
   password: "secret123",
   first_name: "Rabea",
-  last_name: "Badea"
+  last_name: "Badea",
+  facebook_picture_url: "https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/11267855_494475644050145_7650517800177078839_n.jpg?oh=88f598f1fc8228a3a81f204d69ab47bb&oe=5A916072"
 )
 
 user_two = User.create!(
   email: "sarah_lafer@yahoo.com",
   password: "secret123",
   first_name: "Sarah",
-  last_name: "Lafer"
+  last_name: "Lafer",
+  facebook_picture_url: "https://scontent-frt3-1.xx.fbcdn.net/v/t31.0-8/15123387_1524489700911298_8540080221667972968_o.jpg?oh=81c74c24d54e1d3f4989184e7a9ee45a&oe=5AA0CACD"
 )
 
 user_three = User.create!(
   email: "stephan_is_great@gmail.com",
   password: "secret123",
   first_name: "Stephan",
-  last_name: "Schmidbauer"
+  last_name: "Schmidbauer",
+  facebook_picture_url: "https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/12472516_1150297835028069_8300065499297260894_n.jpg?oh=61fd7f780e66b7d600be2918c009e714&oe=5AA8DD43"
 )
 
 user_four = User.create!(
   email: "richard_ogrady@gmail.com",
   password: "secret123",
   first_name: "Richard",
-  last_name: "O'Grady"
+  last_name: "O'Grady",
+  facebook_picture_url: "https://scontent-frt3-1.xx.fbcdn.net/v/t31.0-8/15972751_237246466721822_1634594414421206870_o.jpg?oh=51c4df723a1a2e64c7972c3e34468b0b&oe=5A8F41B5"
 )
 
 user_five = User.create!(
   email: "david_beckham@hotmail.com",
   password: "secret123",
   first_name: "David",
-  last_name: "Beckham"
+  last_name: "Beckham",
+  facebook_picture_url: "https://www.famousbirthdays.com/headshots/david-beckham-4.jpg"
 )
 
 users = [user_one, user_two, user_three, user_four, user_five]
 puts users
 
 puts "Users completed"
-
+puts "Creating bikes"
 Bike.create!(
   address: "Schützenstraße 51, 10117 Berlin",
   city: "berlin",
@@ -66,7 +74,7 @@ Bike.create!(
   # longitude: 13.395794,
   )
 
-puts "completed first"
+puts "First bike completed"
 
 Bike.create!(
   address: "Axel-Springer-Straße 41, 10969 Berlin",
@@ -82,7 +90,7 @@ Bike.create!(
   # longitude: 13.399219,
   )
 
-puts "completed 2"
+puts "Second bike completed"
 
 Bike.create!(
   address: "26 Rudi-Dustchke-Strasse, 10969 Berlin",
@@ -98,7 +106,7 @@ Bike.create!(
   # longitude: 13.391423,
   )
 
-puts "3"
+puts "Third bike completed"
 
 Bike.create!(
   address: "Charlottenstraße 85-96, 10969 Berlin",
@@ -113,7 +121,7 @@ Bike.create!(
   # latitude: 52.505938,
   # longitude: 13.392723,
   )
-puts "4... one more"
+puts "Fourth bike completed"
 
 Bike.create!(
   address: "Schützenstraße 8, 10117 Berlin",
@@ -129,6 +137,5 @@ Bike.create!(
   # longitude: 13.392961,
   )
 
-puts "Fuck me you've actually loaded something"
-
-puts "Populating DB"
+puts "Fifth bike completed"
+puts "Created #{User.all.length} Users and #{Bike.all.length} bikes"
